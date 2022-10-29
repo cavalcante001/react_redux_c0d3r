0272bfae1d7abce2d7a64ac1382275bd40fc2f95
Paulo <paulocavalcante001@outlook.com>
2022-10-29 13:19:59 -0300

    limpando a implementação

--- src/component/Intervalo.jsx
+++ src/component/Intervalo.jsx
@@ -4,18 +4,16 @@ import Card from './Card';
 
 export default props => {
 
-    const { min, max } = props;
-
     return (
         <Card title="Intervalo de Números" red>
             <div className="intervalo">
                 <span>
                     <strong>Mínimo</strong>
-                    <input type="number" value={min} onChange={e => props.onMinChange(+e.target.value)}/>
+                    <input type="number" value={0} readOnly />
                 </span>
                 <span>
                     <strong>Máximo</strong>
-                    <input type="number" value={max} onChange={e => props.onMaxChange(+e.target.value)}/>
+                    <input type="number" value={10} readOnly />
                 </span>
             </div>
         </Card>
